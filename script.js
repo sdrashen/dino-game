799fa7680dd3a4780127596c5777c407f6fe1a72
Sabrina Drashen <sdrashen@gmail.com>
2022-04-27 11:16:21 -0300

    removed dino jumping bug

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const dino = document.querySelector('.dino')
+let isJumping = false //Essa var trabalha a questão de ele estar pulando ou não, porque quando o botão space era clicado muitas vezes rápido para pular o dino bugava.
     //As linhas de código abaixo fazem o dino pular
 function handleKeyUp(event) {
     //O event é enviado para a function sempre que pressionada a tecla pelo navegador
     //keyCode é código das teclas no teclado
     if (event.keyCode === 32) {
-        jump()
+        //Aqui insere-se essa negativa, porque se ele não estiver pulando, então é para pular
+        if (!isJumping) {
+            jump()
+        }
     }
 }
 
@@ -12,18 +16,22 @@ function jump() {
     let position = 0 //Posição inicial do dinossauro, em baixo.
         //A var upInterval criará uma repetição de intervalos para ações, que aqui é mover o dino para cima.
         //A function setInterval define intervalos. O que estiver descrito nela será executado sem parar no intervalo que foi definido. 20ms nesse caso
+    isJumping = true
     let upInterval = setInterval(() => {
         //Esse if é para impedir que ele saia voando
         if (position >= 150) {
             //clearInterval éfunction do JS
             clearInterval(upInterval)
-                //Para o dino descer
+
+            //Para o dino descer
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval)
+                    isJumping = false //Aqui determina que ele não está mais pulando
+                } else {
+                    position -= 20
+                    dino.style.bottom = position + 'px'
                 }
-                position -= 20
-                dino.style.bottom = position + 'px'
             }, 20)
         } else {
             //Sempre que o intervalo repetir ele vai pegar o valor que está na posição e incrementar 20.
