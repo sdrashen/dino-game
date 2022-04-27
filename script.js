0e91f08c28611cce62f70b2fae870d0cf04813c3
Sabrina Drashen <sdrashen@gmail.com>
2022-04-27 11:06:41 -0300

    created jump animation

--- script.js
+++ script.js
@@ -4,8 +4,33 @@ function handleKeyUp(event) {
     //O event é enviado para a function sempre que pressionada a tecla pelo navegador
     //keyCode é código das teclas no teclado
     if (event.keyCode === 32) {
-        console.log('Pressed space key')
+        jump()
     }
+}
+
+function jump() {
+    let position = 0 //Posição inicial do dinossauro, em baixo.
+        //A var upInterval criará uma repetição de intervalos para ações, que aqui é mover o dino para cima.
+        //A function setInterval define intervalos. O que estiver descrito nela será executado sem parar no intervalo que foi definido. 20ms nesse caso
+    let upInterval = setInterval(() => {
+        //Esse if é para impedir que ele saia voando
+        if (position >= 150) {
+            //clearInterval éfunction do JS
+            clearInterval(upInterval)
+                //Para o dino descer
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval)
+                }
+                position -= 20
+                dino.style.bottom = position + 'px'
+            }, 20)
+        } else {
+            //Sempre que o intervalo repetir ele vai pegar o valor que está na posição e incrementar 20.
+            position += 20 //Mesmo que positon = position + 20
+            dino.style.bottom = position + 'px'
+        }
+    }, 20)
 }
 document.addEventListener('keyup', handleKeyUp)
     //Keyup: quando a tecla é pressionada, um evento de keydown é gerado. Quando a tecla é liberada, o keyup (subir) acontece.
